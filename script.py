cc33dc67793fe2072935b7b873743711e17bbd2b
Diego Ramos Claumann <dclaumanndeveloper@protonmail.com>
2024-08-03 16:09:00 -0300

    criação de script para predição

---
+++ script.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import pickle
+
+df = pd.read_csv('dataset.csv')
+
+#criar o modelo de ML
+
+modelo = linear_model.LinearRegression()
+
+x = df[['m2']]
+y = df[['valor']]
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x,y)
+
+# treinando o modelo de ML
+modelo.fit(x_treino,y_treino)
+
+previsoes = modelo.predict(x_teste)
+
+#testar o modelo
+acuracidade =  round(r2_score(y_teste,previsoes) * 100,2)
+print(f"A acuracidade do modelo é de {acuracidade}%")
+
+
+
+
+with open("modelo_treinado.pkl","wb") as file:
+    pickle.dump(modelo,file)
